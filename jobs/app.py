9feab5540289014c21fdbf52848e570ebf7b96f7
Josh Lintag <jlintag@gmail.com>
2020-03-27 15:11:29 -0400

    added db connection, making pluralsight work

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, g
 import sqlite3
 
-PATH = '../db/jobs.sqlite'
+PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
@@ -17,7 +17,7 @@ def open_connection():
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
-    if commit:
+    if commit == True:
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
